da3592f023fbfb0c8a90f3d0ec92435801d1d5a2
cat_mood <2timmoff3@gmail.com>
2023-02-27 21:25:11 +0300

    lab21: init

---
+++ lab21.sh
@@ -0,0 +1,55 @@
+#! /usr/bin/env bash
+
+function die() {
+    echo "$@" >&2
+    exit 1
+}
+
+num_of_copies=
+suffix=1
+
+if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
+    echo "Using: mcp [ARGS] NUMBER_OF_COPIES SOURCE DIRECTORY"
+    echo "ARGUMENTS:"
+    echo "-h, --help       display this help and exit"
+    echo "-s, --suffix     copy suffix will begin with this number or letter (only capital)"
+    exit 0
+elif [[ "$1" == "-s" ]] || [[ "$1" == "--suffix" ]]; then
+    shift
+    suffix="$1"
+    shift
+fi
+
+if [[ "$1" =~ ^[0-9]+$ ]] && (( "$1" > 0 )); then
+    num_of_copies=$1
+    shift
+else
+    die "Wrong number of copies!"
+fi
+
+filepath="$1"
+shift
+destination="$1"
+shift
+
+if [[ !(-f $filepath) ]]; then
+    die "'$filepath' is not a file"
+fi
+
+if [[ !(-d $destination) ]]; then
+    mkdir "$destination"
+fi
+
+filename="$(basename "$filepath")"
+
+for (( i=0; i<num_of_copies; i++ )); do
+    if [[ "$suffix" =~ ^[0-9]+$ ]]; then
+        cp "$filepath" "$destination"/"${filename%.*}"_"$suffix"."${filename#*.}"
+        (( suffix++ ))
+    elif [[ "$suffix" =~ ^[A-Z]+$ ]]; then
+        cp "$filepath" "$destination"/"${filename%.*}"_"$suffix"."${filename#*.}"
+        suffix="$(tr '[A-Y]' '[B-Z]' <<< "$suffix")"
+    else
+        die "Wrong suffix!"
+    fi 
+done
