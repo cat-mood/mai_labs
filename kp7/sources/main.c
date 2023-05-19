931073e2215a1cb0cfa5bcd2c056c2f3237a9df7
cat_mood <2timmoff3@gmail.com>
2023-05-19 11:09:35 +0300

    Fixed negative numbers

--- kp7/sources/main.c
+++ kp7/sources/main.c
@@ -5,9 +5,13 @@
 int input_matr(dbl_vec* CIP, dbl_vec* PI, dbl_vec* YE){
     int width = 0, c = getchar();
     int num = 0, column = 0, not_zeros = 0;
+    bool is_negative = false;
     while (c != EOF){
         if (c == ' '){
             if (num != 0){
+                if (is_negative) {
+                    num *= -1;
+                }
                 push_back(YE, num);
                 push_back(PI, column);
                 not_zeros++;
@@ -15,17 +19,17 @@ int input_matr(dbl_vec* CIP, dbl_vec* PI, dbl_vec* YE){
             // как-то надо определить первый элекмент в линии: длина всего вектора PI - количество элементов в текущей строке
             column++;
             num = 0;
+            is_negative = false;
         } else if (c == '\n' || c == EOF) {
             if (num != 0){
+                if (is_negative) {
+                    num *= -1;
+                }
                 push_back(YE, num);
                 push_back(PI, column);
                 not_zeros++;
             }
-            // if (not_zeros == 0) {
-            //     push_back(CIP, -1);
-            // } else {
             push_back(CIP, get_size(PI) - not_zeros);
-            // }
             not_zeros = 0;
             column++;
             if (column > width) {
@@ -33,6 +37,9 @@ int input_matr(dbl_vec* CIP, dbl_vec* PI, dbl_vec* YE){
             }
             column = 0;
             num = 0;
+            is_negative = false;
+        } else if (c == '-') {
+            is_negative = true;
         } else if (!isdigit(c)){
             fprintf(stderr, "Not number was inputed!\n");
             return -1;
