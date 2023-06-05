7825bc730a48e7603cfc412ce28ad9d0bc0629be
cat_mood <2timmoff3@gmail.com>
2023-06-05 11:02:16 +0300

    kp8: add text interface

--- kp8/sources/main.c
+++ kp8/sources/main.c
@@ -19,29 +19,82 @@ void delete_in_range(list* l, uint start, uint end) {
     } while (!iter_eq(it, iter_begin(l)) || is_first_deleted);
 }
 
-int main() {
-    list l = new_list();
-    int n;
-    printf("Enter number of elements in the list:\n");
-    scanf("%d", &n);
-    printf("Fill the list:\n");
+void add_menu(list* l) {
+    iter it = iter_begin(l);
+    printf("Enter idx of element: ");
+    int idx;
+    scanf("%d", &idx);
+    if ((l->size == 0 && idx != 0) || (l->size != 0 && (idx > l->size - 1 || idx < 0))) {
+        printf("Wrong idx!\n");
+        return;
+    }
+    printf("Enter number: ");
     uint el;
-    iter it = iter_begin(&l);
-    for (int i = 0; i < n; i++) {
-        scanf("%u", &el);
-        add_el(&it, el);
+    scanf("%u", &el);
+    for (int i = 0; i < idx; i++) {
         iter_inc(&it);
     }
-    it = iter_begin(&l);
-    printf("List:\n");
-    print_list(&l);
-    printf("Enter range (start, end):\n");
-    uint start, end;
-    scanf("%u", &start);
-    scanf("%u", &end);
-    delete_in_range(&l, start, end);
-    printf("List:\n");
-    print_list(&l);
+    add_el(&it, el);
+}
+
+void delete_menu(list* l) {
+    iter it = iter_begin(l);
+    printf("Enter idx of element: ");
+    int idx;
+    scanf("%d", &idx);
+    if (idx > l->size - 1 || idx < 0) {
+        printf("Wrong idx!\n");
+        return;
+    }
+    for (int i = 0; i < idx; i++) {
+        iter_inc(&it);
+    }
+    delete_el(&it);
+}
+
+void delete_in_range_menu(list* l) {
+    printf("Enter range [a; b):\n");
+    uint left;
+    scanf("%u", &left);
+    uint right;
+    scanf("%u", &right);
+    delete_in_range(l, left, right);
+}
+
+void menu(list* l) {
+    int c;
+    do {
+        printf("Options: \n");
+        printf("1. Add element\n");
+        printf("2. Delete element\n");
+        printf("3. Delete in range\n");
+        printf("4. Print list\n");
+        printf("5. Exit\n");
+        printf("\nChoose option: ");
+        c = getchar();
+        if (c == '1') {
+            add_menu(l);
+        } else if (c == '2') {
+            delete_menu(l);
+        } else if (c == '3') {
+            delete_in_range_menu(l);
+        } else if (c == '4') {
+            print_list(l);
+        }  else if (c == '5' || c == EOF) {
+            printf("Goodbye!\n");
+            break;
+        } else {
+            printf("Wrong option!\n");
+        }
+        while (c != '\n') {
+            c = getchar();
+        }
+    } while (c != '5');
+}
+
+int main() {
+    list l = new_list();
+    menu(&l);
     destroy_list(&l);
 
     return 0;
